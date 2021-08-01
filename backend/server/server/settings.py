c0c78bf2230f7c5f0eef8dc3b43aba6ce39945e1
rivalnofirm <rivalnof2@gmail.com>
2021-08-02 02:55:20 +0700

    docker file update

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-34@kgg@fj)14m%r^+$k8-$qe(y9#8gn0=+agtr6s_3)b#%wvlm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
@@ -120,7 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
